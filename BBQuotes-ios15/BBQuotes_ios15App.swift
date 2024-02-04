f27360a9cb1cda7061e65ca0e39d04ee209c1e2a
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-04 14:19:29 +0200

    Initial Commit

---
+++ BBQuotes-ios15/BBQuotes_ios15App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes_ios15App.swift
+//  BBQuotes-ios15
+//
+//  Created by Alin RADU on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes_ios15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
